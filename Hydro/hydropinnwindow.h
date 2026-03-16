a9eb1570079c961b2be4311d4e7cdaa4ee2a5453
Behzad Shakouri <behzad.shakouri92@gmail.com>
2026-03-16 15:29:37 -0400

    Clarify Hydro mode implementations vs NeuroForge naming

--- Hydro/hydropinnwindow.h
+++ Hydro/hydropinnwindow.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QString>
+
+#include <map>
 
 #include "models/hydro_run_types.h"
 
@@ -13,6 +16,8 @@ class QDoubleSpinBox;
 class QCheckBox;
 class QLineEdit;
 class QChartView;
+class QTextBrowser;
+class QListWidget;
 
 /**
  * @file hydropinnwindow.h
@@ -27,14 +32,9 @@ public:
 private:
     QLabel* statusLabel_;
     QComboBox* modeCombo_;
-    QPushButton* runButton_;
-    QPushButton* runAllButton_;
-    QPushButton* runFFNButton_;
-    QPushButton* runFFNPINNButton_;
-    QPushButton* runLSTMButton_;
-    QPushButton* runLSTMPINNButton_;
     QTextEdit* logText_;
     QChartView* chartView_;
+    QTextBrowser* perfSummaryText_;
 
     // Training/network controls
     QSpinBox* epochsSpin_;
@@ -45,7 +45,25 @@ private:
     QDoubleSpinBox* physicsWeightSpin_;
     QLineEdit* hiddenLayersEdit_;
     QComboBox* activationCombo_;
+    QSpinBox* layerSizeSpin_;
+    QComboBox* layerActivationCombo_;
+    QPushButton* addLayerButton_;
+    QPushButton* removeLayerButton_;
+    QListWidget* layersList_;
+    QComboBox* outputActivationCombo_;
     QCheckBox* evalCheck_;
+    QDoubleSpinBox* splitRatioSpin_;
+    QCheckBox* shuffleCheck_;
+    QSpinBox* seedSpin_;
+    QComboBox* optimizerCombo_;
+    QDoubleSpinBox* weightDecaySpin_;
+    QDoubleSpinBox* momentumSpin_;
+    QComboBox* normalizationCombo_;
+    QCheckBox* incrementalCheck_;
+    QDoubleSpinBox* windowSizeSpin_;
+    QDoubleSpinBox* windowStepSpin_;
+    QSpinBox* epochsPerWindowSpin_;
+    QCheckBox* resetOptimizerWindowCheck_;
 
     // Data controls
     QComboBox* dataSourceCombo_;
@@ -54,6 +72,7 @@ private:
     QSpinBox* csvXColSpin_;
     QSpinBox* csvYColSpin_;
     QCheckBox* csvHeaderCheck_;
+    QPushButton* useNeuroforgeCsvPresetButton_;
     QSpinBox* sampleCountSpin_;
     QDoubleSpinBox* tStartSpin_;
     QDoubleSpinBox* tEndSpin_;
@@ -62,10 +81,46 @@ private:
     QLineEdit* syntheticExportPathEdit_;
     QPushButton* browseSyntheticExportButton_;
 
+    // NeuroForge-style workflow actions
+    QPushButton* runPredictionButton_;
+    QPushButton* runAllPredictionButton_;
+    QPushButton* runPredictionFFNButton_;
+    QPushButton* runPredictionFFNPINNButton_;
+    QPushButton* runPredictionLSTMButton_;
+    QPushButton* runPredictionLSTMPINNButton_;
+    QCheckBox* predictionUseCurrentDataCheck_;
+    QPushButton* runTrainingButton_;
+    QPushButton* runAllTrainingButton_;
+    QPushButton* runTrainingFFNButton_;
+    QPushButton* runTrainingFFNPINNButton_;
+    QPushButton* runTrainingLSTMButton_;
+    QPushButton* runTrainingLSTMPINNButton_;
+    QPushButton* configureGAButton_;
+    QPushButton* startGAButton_;
+    QPushButton* stopGAButton_;
+    QPushButton* refreshPerformanceButton_;
+    QPushButton* clearPlotButton_;
+    QPushButton* showInputsOutputsButton_;
+    QPushButton* zoomInPlotButton_;
+    QPushButton* zoomOutPlotButton_;
+    QPushButton* fitPlotButton_;
+    QPushButton* plotAllTargetPredButton_;
+    QPushButton* plotOneToOneButton_;
+    QPushButton* plotTaylorButton_;
+    std::map<QString, HydroRunResult> lastModeResults_;
+    std::vector<double> lastSyntheticX_;
+    std::vector<double> lastSyntheticTarget_;
+    std::map<QString, std::vector<double>> lastSyntheticInputs_;
+
     void updateStatus();
     void runSelectedMode();
     void runAllModes();
     void runMode(const QString& mode);
+    void showSelectedPrediction();
+    void showAllPredictions();
+    void showPredictionForMode(const QString& mode);
+    void showSyntheticInputsOutputs();
+    void applyNeuroforgeCsvPreset();
     void setRunningUiState(bool running);
     void updateDataSourceUiState();
     void browseCsv();
@@ -73,5 +128,20 @@ private:
     void generateSyntheticDataPreview();
     void appendLog(const QString& line);
     HydroRunConfig currentConfig() const;
+    QString selectedModeKey() const;
+    void syncNetworkCsvFromLayerList();
+    std::vector<QString> configuredLayerActivations() const;
     void updatePlot(const QString& mode, const HydroRunResult& result);
+    void configureGAPlaceholder();
+    void startGAPlaceholder();
+    void stopGAPlaceholder();
+    void refreshPerformanceAssessment();
+    void clearPlot();
+    void zoomInPlot();
+    void zoomOutPlot();
+    void fitPlotAxes();
+    void fitPlotAxesInternal(bool logMessage);
+    void plotAllTargetVsPredicted();
+    void plotOneToOneAllModes();
+    void plotTaylorDiagramAllModes();
 };
